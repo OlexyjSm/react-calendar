4b97a8ac94fc5233dc5df37d8cb4ebe966bd3ee1
OlexyjSm <115574406+OlexyjSm@users.noreply.github.com>
2024-10-11 17:47:09 +0300

    Create Calendar.js
    Видалено формат відображення тижня, додано перемикач на темний режим

---
+++ Calendar.js
@@ -0,0 +1,12 @@
+<div className="toggle-container">
+          <label className="switch">
+            <input
+              type="checkbox"
+              checked={isDarkMode}
+              onChange={toggleDarkMode}
+            />
+            <span className="slider"></span>
+          </label>
+          <span>Темний режим</span>
+        </div>
+      </div>
